cf3e921db89e02aa10c98b3713bd252319897985
Alli Neuman <alli.neuman@gmail.com>
2019-09-18 08:17:11 -0500

    Add program files
    Add txt file that includes a list of test strings one on each line.
    Add python file that takes in txt file, reads it and returns each row of permutations sorted in alphabetical order ascending.

---
+++ permute.py
@@ -0,0 +1,28 @@
+f = open("str.txt", "r")
+if f.mode == 'r':
+    lines = f.read().splitlines()
+    # print lines
+
+    # finding and returning all permutations
+    # where char are individual string characters, l is the starting index and r is the ending index
+    def permute(char, l, r, permute_list):
+        if l==r:
+            permutation = ''.join(char)
+            permute_list.append(permutation)
+            permute_list.sort()
+
+        else:
+            for j in xrange(l, r+1):
+                char[l], char[j] = char[j], char[l]
+                permute(char, l+1, r, permute_list)
+                char[l], char[j] = char[j], char[l]
+
+    #line is the original line of the text file
+    #n are how many string characters are in i
+    #char are the individual strings that make up i
+    for line in lines:
+        permute_list = []
+        n = len(line)
+        char = list(line)
+        permute(char, 0, n-1, permute_list)
+        print ', '.join(permute_list)
